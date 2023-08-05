ce03b2f6d2fb680f1c59feec1efdeda4bbac09bd
NickShuster <124299055+NickShuster@users.noreply.github.com>
2023-08-05 15:59:05 +0200

    11

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
 
-import React from 'react';
-import Profile from './Profile/Profile';
+import React, { useState, useEffect } from 'react';
+import Profile from './Profile/Profile'; 
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory';
+
 import user from '../components/user.json'; 
+import data from '../components/data.json';
+import transactions from '../components/transactions.json';
+import friends from '../components/friends.json'; 
 
 const App = () => {
+  const [friendsData, setFriendsData] = useState([]);
+
+  useEffect(() => {
+    setFriendsData(friends);
+  }, []);
+
   return (
     <div>
       <h1>My Social Media Profile</h1>
@@ -14,6 +27,12 @@ const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
+      <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends={friendsData} />
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
